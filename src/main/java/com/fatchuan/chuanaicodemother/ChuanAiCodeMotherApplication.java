8280205b21bed4e81c8348b3ab9ad77b6c1fa086
fatchuan <499252411@qq.com>
2025-09-22 12:24:05 +0800

    初始化后端框架

---
+++ src/main/java/com/fatchuan/chuanaicodemother/ChuanAiCodeMotherApplication.java
@@ -0,0 +1,13 @@
+package com.fatchuan.chuanaicodemother;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuanAiCodeMotherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChuanAiCodeMotherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fatchuan/chuanaicodemother/ChuanAiCodeMotherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatchuan.chuanaicodemother;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuanAiCodeMotherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
