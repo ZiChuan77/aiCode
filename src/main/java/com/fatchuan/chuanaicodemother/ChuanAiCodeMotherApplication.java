322baef32b4e18851853ca5ff30ac933599c9c7a
fatchuan <499252411@qq.com>
2025-09-24 11:51:32 +0800

    用户模块后端 开发 MyBatis Flex 代码生成器 开发核心功能，比如用户注册、用户登录、获取登录用户信息、用户注销等 全局权限校验 AOP 开发管理员增删改查接口 自定义配置解决丢失精度问题

--- src/main/java/com/fatchuan/chuanaicodemother/ChuanAiCodeMotherApplication.java
+++ src/main/java/com/fatchuan/chuanaicodemother/ChuanAiCodeMotherApplication.java
@@ -1,9 +1,11 @@
 package com.fatchuan.chuanaicodemother;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.fatchuan.chuanaicodemother.mapper")
 public class ChuanAiCodeMotherApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/fatchuan/chuanaicodemother/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.fatchuan.chuanaicodemother.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.fatchuan.chuanaicodemother.model.entity.User;
+
+/**
+ * 用户 映射层。
+ *
+ * @author <a href="https://github.com/ZiChuan77">fatchuan</a>
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
